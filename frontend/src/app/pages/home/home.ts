0ceea9a7aa588d4f2dad2ab236398b881fc6a8cf
igord <igordf2502@gmail.com>
2026-06-17 21:26:03 -0300

    feat: componente home page

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EstoqueService } from '../../services/estoque.service';
+import { ProdutoSaldoDTO, AlertaDTO } from '../../models/estoque.models';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  templateUrl: './home.html',
-  styleUrl: './home.scss',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
-export class Home {}
+export class HomeComponent implements OnInit {
+  produtosCriticos: ProdutoSaldoDTO[] = [];
+  alertasRecentes: AlertaDTO[] = [];
+
+  constructor(private estoqueService: EstoqueService) {}
+
+  ngOnInit(): void {
+    this.carregarDados();
+  }
+
+  carregarDados(): void {
+    this.estoqueService.listarProdutosCriticos().subscribe((dados) => {
+      this.produtosCriticos = dados;
+    });
+
+    this.estoqueService.listarAlertas(false).subscribe((dados) => {
+      this.alertasRecentes = dados.slice(0, 5);
+    });
+  }
+}
